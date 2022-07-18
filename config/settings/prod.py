085219ac878fe63bbfa0ed3a0f48b0230e20ffa9
yuelleta <mg09091234@gmail.com>
2022-07-18 14:17:39 +0900

    fix 502

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
 
 ALLOWED_HOSTS = ['43.200.127.161']
-DEBUG = False
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
+
 from pybo.models import Question
 
 
@@ -12,7 +13,7 @@ def index(request):
         question_list = question_list.filter(
             Q(subject__icontains=kw) |  # 제목
             Q(content__icontains=kw) |  # 내용
-            Q(answer__content__icontains=kw) |  # 답변 내용
+            Q(answer__content__icontains=kw) |  # 답변내용
             Q(author__username__icontains=kw) |  # 질문 글쓴이
             Q(answer__author__username__icontains=kw)  # 답변 글쓴이
         ).distinct()
